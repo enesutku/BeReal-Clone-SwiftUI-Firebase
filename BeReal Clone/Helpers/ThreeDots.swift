32c158d7b236f93ee489a4e91d7cdb9a7be7cd44
Enes UTKU <ensutku@hotmail.com>
2024-06-04 23:59:51 +0300

    Update

---
+++ BeReal Clone/Helpers/ThreeDots.swift
@@ -0,0 +1,29 @@
+// Created by Enes UTKU
+
+import SwiftUI
+
+struct ThreeDots: View {
+    
+    var size: CGFloat
+    var color: Color
+    
+    var body: some View {
+        HStack {
+            Image(systemName: "circle.fill")
+                .foregroundStyle(color)
+                .font(.system(size: size))
+            Image(systemName: "circle.fill")
+                .foregroundStyle(color)
+                .font(.system(size: size))
+                .padding(.leading, -4)
+            Image(systemName: "circle.fill")
+                .foregroundStyle(color)
+                .font(.system(size: size))
+                .padding(.leading, -4)
+        }
+    }
+}
+
+#Preview {
+    ThreeDots(size: 12, color: .black)
+}
--- BeReal Clone/Views/Feed/FeedView.swift
+++ BeReal Clone/Views/Feed/FeedView.swift
@@ -9,6 +9,63 @@ struct FeedView: View {
                 .ignoresSafeArea()
             
             ZStack {
+                
+                ScrollView(showsIndicators: false) {
+                    VStack {
+                        VStack {
+                            ZStack {
+                                VStack(alignment: .leading) {
+                                    Image("back")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .clipShape(.rect(cornerRadius: 5))
+                                }
+                                
+                                VStack {
+                                    HStack {
+                                        Image("front")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .border(.black)
+                                            .clipShape(.rect(cornerRadius: 2))
+                                            .frame(width: 20, height: 40)
+                                            .padding(.leading)
+                                        Spacer()
+                                    }
+                                    .padding(.top, 18)
+                                    Spacer()
+                                }
+                            }
+                            .frame(width: 100)
+                        }
+                        
+                        VStack {
+                            Text("Add a caption...")
+                                .foregroundStyle(.white)
+                                .fontWeight(.semibold)
+                            Text("View Comment")
+                                .foregroundStyle(.gray)
+                            
+                            HStack {
+                                Text("Home •  1 hr late")
+                                    .foregroundStyle(.gray)
+                                    .font(.system(size: 12))
+                                ThreeDots(size: 3, color: .gray)
+                            }
+                        }
+                        
+                        ForEach(1..<8) { _ in
+                        FeedCellView()
+                        }
+                        
+                    }
+                    .padding(.top, 80)
+                }
+                
+                
+                
+                
+                
                 VStack {
                     VStack {
                         HStack {
@@ -43,7 +100,6 @@ struct FeedView: View {
                     
                     Spacer()
                     
-                    FeedCellView()
                     
                 }
             }
